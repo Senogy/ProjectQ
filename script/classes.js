c563c4a221d3dd5fa2db095dc0871dcacf85e314
Theo Bonnard <theo.bonnard@supelec.fr>
2020-04-26 14:27:29 +0200

    Lint

--- script/classes.js
+++ script/classes.js
@@ -1,5 +1,5 @@
-class Classe{
-  constructor(ClassStats){
+class Classe {
+  constructor(ClassStats) {
     this.name = ClassStats[0];      // Classe name
     this.hp = ClassStats[1];        // Health point
     this.str = ClassStats[2];       // Strenght
@@ -10,89 +10,89 @@ class Classe{
     this.blkchance = ClassStats[7]; // Block chance
   }
 
-  attack(){
+  attack () {
     let bestStat;
-    bestStat = Math.max(this.str,this.agi,this.int); // Determinate best offensive statistic between strength agility and intelligence
-    return Math.floor((1+Math.random()) * (bestStat/3)); // deal damage between half and full of the stats
+    bestStat = Math.max(this.str, this.agi, this.int); // Determinate best offensive statistic between strength agility and intelligence
+    return Math.floor((1 + Math.random()) * (bestStat / 3)); // deal damage between half and full of the stats
   }
 
-  dodge(){
+  dodge () {
     let dodgeroll, dodgechance;
-    dodgechance = this.agi/250;
+    dodgechance = this.agi / 250;
     dodgeroll = Math.random();
-    if (dodgeroll < dodgechance){
+    if (dodgeroll < dodgechance) {
       return 1;
     }
-    else{
+    else {
       return 0;
     }
   }
-  crit(name){};
+  crit (name) { };
 }
-class Warrior extends Classe{
-  constructor(){
-    super(["Warrior",150,40,10,20,0.2,1.5,0.2]);
+class Warrior extends Classe {
+  constructor() {
+    super(["Warrior", 150, 40, 10, 20, 0.2, 1.5, 0.2]);
   }
 
-  dodge(){
+  dodge () {
     let dodgeroll, dodgechance, blockroll;
-    dodgechance = this.agi/250;
+    dodgechance = this.agi / 250;
     dodgeroll = Math.random();
     blockroll = Math.random();
-    if (dodgeroll < dodgechance){
+    if (dodgeroll < dodgechance) {
       return 1;
     }
-    else{
-      if(blockroll < this.blkchance){
+    else {
+      if (blockroll < this.blkchance) {
         return 2;
       }
-      else{
+      else {
         return 0;
       }
     }
   }
 }
-class Archer extends Classe{
-  constructor(){
-    super(["Archer",130,30,50,20,0.33,1.25,0]);
+class Archer extends Classe {
+  constructor() {
+    super(["Archer", 130, 30, 50, 20, 0.33, 1.25, 0]);
   }
 
-  crit(name){
-    battleLog.innerHTML += "<br />The "+name+ " "+this.name+" quickly fire his next arrow !";
+  crit (name) {
+    battleLog.innerHTML += "<br />The " + name + " " + this.name + " quickly fire his next arrow !";
     attackTurn(name);
   }
 }
 class Assassin extends Classe {
   constructor() {
-    super(["Assassin",120,10,40,20,0.30,3,0]);
+    super(["Assassin", 120, 10, 40, 20, 0.30, 3, 0]);
   }
 }
 class Mage extends Classe {
   constructor() {
-    super(["Mage",100,20,30,70,0.05,3,0]);
+    super(["Mage", 100, 20, 30, 70, 0.05, 3, 0]);
   }
 }
 class Healer extends Classe {
   constructor() {
-    super(["Healer",140,30,30,40,0.15,1.5,0]);
+    super(["Healer", 140, 30, 30, 40, 0.15, 1.5, 0]);
   }
 
-  crit(name){
-    let healroll = Math.floor((1+Math.random()) * (this.int/2));
+  crit (name) {
+    let healroll = Math.floor((1 + Math.random()) * (this.int / 2));
     this.hp += healroll; //if Healer crit he heal himself for half of the damage dealt dodged or not
-    battleLog.innerHTML += "<br />The "+name+" "+this.name+" healed himself of "+healroll+" Hp !";
+    battleLog.innerHTML += "<br />The " + name + " " + this.name + " healed himself of " + healroll + " Hp !";
   }
 }
 class Illusionist extends Classe {
   constructor() {
-    super(["Illusionist",110,20,20,40,0.5,1.2,0]);
+    super(["Illusionist", 110, 20, 20, 40, 0.5, 1.2, 0]);
   }
 
-  crit(name){
+  crit (name) {
     let hpSwap;
     hpSwap = nemesis.hp;
     nemesis.hp = hero.hp;
     hero.hp = hpSwap;
-    battleLog.innerHTML += "<br />The "+name+" "+this.name+" just did a life swap";
+    battleLog.innerHTML += "<br />The " + name + " " + this.name + " just did a life swap";
   }
 }
--- script/setupfunction.js
+++ script/setupfunction.js
@@ -1,9 +1,9 @@
-function setupGame(charclass){     // Charclass répare charclass remplace
+function setupGame (charclass) {     // Charclass répare charclass remplace
   document.getElementById('ClassSelector').style.display = "none";
   console.log("ProjectQ now running...");
   nemesisroll = getRandomClass();
-  hero = charselect(""+charclass);
-  nemesis = charselect(""+nemesisroll);
+  hero = charselect("" + charclass);
+  nemesis = charselect("" + nemesisroll);
   document.getElementById('GameWindow').style.display = "flex";
   document.getElementById('HeroClass').innerHTML = hero.name;
   document.getElementById('HeroStats').innerHTML = statWindow(hero);
@@ -12,22 +12,22 @@ function setupGame(charclass){     // Charclass répare charclass remplace
 
 }
 
-function charselect(char){
-  switch(char){
-    case 'Warrior':return new Warrior();break;
-    case 'Archer':return new Archer();break;
-    case 'Assassin':return new Assassin();break;
-    case 'Mage':return new Mage();break;
-    case 'Healer':return new Healer();break;
-    case 'Illusionist':return new Illusionist();break;
-    default:console.log("Wrong Input");
+function charselect (char) {
+  switch (char) {
+    case 'Warrior': return new Warrior(); break;
+    case 'Archer': return new Archer(); break;
+    case 'Assassin': return new Assassin(); break;
+    case 'Mage': return new Mage(); break;
+    case 'Healer': return new Healer(); break;
+    case 'Illusionist': return new Illusionist(); break;
+    default: console.log("Wrong Input");
   }
 }
 
-function getRandomClass(){
+function getRandomClass () {
   var carole; // character roll --> charroll --> Carole
-  var classArray = ["Warrior","Archer","Assassin","Mage","Healer","Illusionist"];
+  var classArray = ["Warrior", "Archer", "Assassin", "Mage", "Healer", "Illusionist"];
   carole = Math.floor(Math.random() * 6);
-  console.log("getRandomClass roll "+carole+" , "+classArray[carole]);
+  console.log("getRandomClass roll " + carole + " , " + classArray[carole]);
   return classArray[carole];
 }
